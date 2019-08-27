4a3d0e6984e27d0ed86ce79c875459495727db8b
Jeremy Karlsson <karlsson@jeremy.se>
2019-08-27 20:06:16 +0200

    Add helpers from corine-eu-api

--- helpers.js
+++ helpers.js
@@ -1,4 +1,20 @@
 /**
+ * @typedef {DecodedOpenLocationCode}
+ * @prop {number} latitudeCenter
+ * @prop {number} longitudeCenter
+ * @prop {number} latitudeLo
+ * @prop {number} longitudeLo
+ * @prop {number} longitudeHi
+ * @prop {number} latitudeHi
+ */
+
+ /**
+ * @typedef {ArcGISPolygon}
+ * @prop {Array.<number[]} rings
+ * @prop {number} spatialReference
+ */
+
+/**
  * Converts latitude and longitue degrees to WSG84 meters.
  *
  * @param {number} longitude
@@ -12,4 +28,36 @@ export function degreesToMeters(longitude, latitude) {
   y = y * 20037508.34 / 180;
 
   return { x, y };
+}
+
+/**
+ * @param {DecodedOpenLocationCode} decodedOpenLocationCode
+ * @returns {{ xmin: number, ymin: number, xmax: number, ymax: number }}
+ */
+export function getBoundsInMeters(decodedOpenLocationCode) {
+  const { x: xmin, y: ymin } = degreesToMeters(decodedOpenLocationCode.longitudeLo, decodedOpenLocationCode.latitudeLo);
+  const { x: xmax, y: ymax } = degreesToMeters(decodedOpenLocationCode.longitudeHi, decodedOpenLocationCode.latitudeHi);
+
+  return { xmin, ymin, xmax, ymax };
+}
+
+/**
+ * @param {DecodedOpenLocationCode} decodedOpenLocationCode
+ * @returns {ArcGISPolygon}
+ */
+export function getArcGISPolygon(decodedOpenLocationCode) {
+  const { xmin, ymin, xmax, ymax } = getBoundsInMeters(decodedOpenLocationCode);
+
+  const wsg84geoJSONPolygon = {
+    type: 'Polygon',
+    coordinates: [[
+      [xmin, ymin],
+      [xmax, ymin],
+      [xmax, ymax],
+      [xmin, ymax],
+      [xmin, ymin]
+    ]]
+  };
+
+  return Object.assign({}, geojsonToArcGIS(wsg84geoJSONPolygon), { spatialReference: 102100 });
 }
